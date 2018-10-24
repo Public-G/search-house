d223c2172c839d1d2a59a7864022d72239cf8db2
lzs <lzs@gmail.com>
2018-10-25 00:21:38 +0800

    :construction:基于RBAC权限模型Controller

---
+++ src/main/java/com/github/modules/sys/controller/SysMenuController.java
@@ -0,0 +1,17 @@
+package com.github.modules.sys.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 系统菜单
+ *
+ * @author ZEALER
+ * @date 2018-10-22
+ */
+@RestController
+@RequestMapping("/sys/menu")
+public class SysMenuController {
+
+
+}
---
+++ src/main/java/com/github/modules/sys/controller/SysRoleController.java
@@ -0,0 +1,15 @@
+package com.github.modules.sys.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 系统角色
+ *
+ * @author ZEALER
+ * @date 2018-10-22
+ */
+@RestController
+@RequestMapping("/sys/role")
+public class SysRoleController {
+}
